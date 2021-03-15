c60587969a24549ba7d96bf8ea1320a75f4f5c1a
bbagchi97 <bagchibikramadittya@gmail.com>
2021-03-15 11:23:42 +0530

    Minor fix for q8.py

--- Assignment 5/q8.py
+++ Assignment 5/q8.py
@@ -10,7 +10,7 @@ print("Insert elements for array: ")
 for i in range(row):
     for j in range(col):
         print(f"Position {i+1}, {j+1}: ", end=' ')
-        inp = input("Enter value: ")
+        inp = int(input("Enter value: "))
         arr1[i][j] = inp
 
 print(arr1)
